c98349dfb8e6861e34d8845dd03c9f5fbcc7740e
vishak <vishak.cretaceous@gmail.com>
2025-01-11 13:54:55 +0530

    changed placeholder text to js variables

--- forumdata.js
+++ forumdata.js
@@ -1,4 +1,4 @@
-var thread = [
+var threads = [
     {
     title: "Is a reccesion coming?",
     author: "Dr. Ryan Dennis Gomez, PhD in Economics, Harvard University",
@@ -7,7 +7,23 @@ var thread = [
     comments: [{
         author: "Aarav Naveen",
         date: Date.now(),
-        content: "Ay u loafer give blick or ill sock u big scene u want vuh",
+        content: "Ay u loafer ill sock u big scene u want vuh",
+    },
+    {  author: "Devesh",
+    date: Date.now(),
+    content: "Come fortnite broo",}
+]
+    
+},
+{
+    title: "How much money is rich?",
+    author: "Dr. Ryan Dennis Gomez, PhD in Economics, Harvard University",
+    date: Date.now(),
+    content: "1 trillion USD",
+    comments: [{
+        author: "Aarav Naveen",
+        date: Date.now(),
+        content: "Ay u loafer ill sock u big scene u want vuh",
     },
     {  author: "Devesh",
     date: Date.now(),
@@ -15,4 +31,5 @@ var thread = [
 ]
 
     
-}]
+}
+]
