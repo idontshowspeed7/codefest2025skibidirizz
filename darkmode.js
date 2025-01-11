7e106e7bc292993b1c8c5499dcc2d2bba25cff7a
Build-the-future10 <82577280+Build-the-future10@users.noreply.github.com>
2025-01-11 15:30:41 +0530

    Update darkmode.js

--- darkmode.js
+++ darkmode.js
@@ -1,2 +1,19 @@
 let darkmode=localStorage.getItem('darkmode')
 const themeSwitch=document.getElementByID('mode-switch')
+
+const enableDarkmode=()=> {
+    document.body.classlist.add('darkmode')
+    localStorage.setItem('darkmode', 'active')
+}
+
+const disableDarkmode=()=> {
+    document.body.classlist.remove('darkmode')
+    localStorage.setItem('darkmode', 'null')
+}
+
+if(darkmode ==='active') enableDarkmode()
+
+modeSwitch.addEventListener("click", () =>) {
+  darkmode= localStorage.getItem('darkmode)
+  darkmode !== "active" ?, enableDarkmode() : disableDarkmode() 
+}
