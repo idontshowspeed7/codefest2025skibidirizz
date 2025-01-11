4747668e7ab3b6bec297aeebf3cce33321d23940
vishak <vishak.cretaceous@gmail.com>
2025-01-11 13:41:29 +0530

    started js code variables

---
+++ forumdata.js
@@ -0,0 +1,18 @@
+var thread = [
+    {
+    title: "Is a reccesion coming?",
+    author: "Dr. Ryan Dennis Gomez, PhD in Economics, Harvard University",
+    date: Date.now(),
+    content: "Yes it is",
+    comments: [{
+        author: "Aarav Naveen",
+        date: Date.now(),
+        content: "Ay u loafer give blick or ill sock u big scene u want vuh",
+    },
+    {  author: "Devesh",
+    date: Date.now(),
+    content: "Come fortnite broo",}
+]
+
+    
+}]
