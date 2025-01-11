57fffe880361aef6de8c9ab33bf5224650b4ff0e
Build-the-future10 <82577280+Build-the-future10@users.noreply.github.com>
2025-01-11 15:42:41 +0530

    Update darkmode.js
    debugging

--- darkmode.js
+++ darkmode.js
@@ -1,21 +1,30 @@
 let darkmode = localStorage.getItem('darkmode');
+
 const modeSwitch = document.getElementById('mode-switch');
 
 const enableDarkmode = () => {
-    document.body.classList.add('darkmode'); 
+    document.body.classList.add('darkmode');
     localStorage.setItem('darkmode', 'active');
 };
 
 const disableDarkmode = () => {
-    document.body.classList.remove('darkmode'); 
-    localStorage.setItem('darkmode', 'null');
+    document.body.classList.remove('darkmode');
+    localStorage.setItem('darkmode', 'null'); 
 };
+
 
 if (darkmode === 'active') {
     enableDarkmode();
 }
 
 modeSwitch.addEventListener("click", () => {
-    darkmode = localStorage.getItem('darkmode'); 
-    darkmode !== 'active' ? enableDarkmode() : disableDarkmode();
+
+    darkmode = localStorage.getItem('darkmode');
+
+
+    if (darkmode !== 'active') {
+        enableDarkmode();
+    } else {
+        disableDarkmode();
+    }
 });
