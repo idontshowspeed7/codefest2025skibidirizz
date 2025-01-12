29663196d6b67350bdaa83229f0148dd24d37116
vishak <vishak.cretaceous@gmail.com>
2025-01-12 05:54:13 +0530

    debug

--- serverhost.js
+++ serverhost.js
@@ -4,11 +4,10 @@ const app = express();
 const PORT = 3000;
 const path = require('path');
 
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '.')));
 
-console.log("ioajdioasd");
 
-mongoose.connect('mongodb://localhost:27017/postsDB', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect("mongodb+srv://vishakcretaceous:sewey123@cluster0.wzl2g.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.error('Error connecting to MongoDB:', err));
 
@@ -24,12 +23,10 @@ const postSchema = new mongoose.Schema({
 const Post = mongoose.model('Post', postSchema);
 
 // Get all posts
-app.get('/api/posts', async (req, res) => {
-
-    console.log("endpoint hit");
-
+app.get('/posts', async (req, res) => {
     try {
         const posts = await Post.find();
+        console.log(posts);
         res.json(posts);
     } catch (error) {
         res.status(500).json({ error: 'Failed to fetch posts' });
@@ -37,7 +34,7 @@ app.get('/api/posts', async (req, res) => {
 });
 
 // Create a new post
-app.post('/api/posts', async (req, res) => {
+app.post('/posts', async (req, res) => {
     const { content, user } = req.body;
     const newPost = new Post({
         content,
@@ -56,4 +53,5 @@ app.post('/api/posts', async (req, res) => {
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
+
 });
